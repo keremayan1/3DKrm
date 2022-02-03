ef3d604adba4d5d2d44c3a0c636b2c0881e709d2
Kerem Ayan <keremayan002@gmail.com>
2022-02-03 14:40:54 +0300

    IdentityServiceler Eklendi. HttpContextAccessor Uzerinden useri okuma

---
+++ Core/KRM3D.Core/Services/CoreIdentityService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KRM3D.Core.Services
+{
+    public class CoreIdentityService : ICoreIdentityService
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public CoreIdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetUserId =>
+            _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
---
+++ Core/KRM3D.Core/Services/ICoreIdentityService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KRM3D.Core.Services
+{
+    public interface ICoreIdentityService
+    {
+         string GetUserId { get; }
+    }
+}
