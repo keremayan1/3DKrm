a588d2350d022e3851a3cb19affb365c780b4957
Kerem Ayan <keremayan002@gmail.com>
2022-02-07 15:17:32 +0300

    Catalog Gateway Eklendi.

---
+++ Gateways/KRM3D.Gateway/Program.cs
@@ -0,0 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
+});
+
+builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
+{
+    options.Authority = builder.Configuration["IdentityServerUrl"];
+    options.Audience = "resource_gateway";
+    options.RequireHttpsMetadata = false;
+});
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.Run();
+
