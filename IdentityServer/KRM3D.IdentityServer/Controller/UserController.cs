817f4ffbba882adcd0daa9ed419a0d4e4d80b9c3
Kerem Ayan <keremayan002@gmail.com>
2022-02-07 11:55:48 +0300

    IdentityServer eklendi, çalışıyor

---
+++ IdentityServer/KRM3D.IdentityServer/Controller/UserController.cs
@@ -0,0 +1,60 @@
+﻿using KRM3D.IdentityServer.Dto;
+using KRM3D.IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace KRM3D.IdentityServer.Controller
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub);
+
+            if (userIdClaim == null) return BadRequest();
+
+            var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+            if (user == null) return BadRequest();
+
+            return Ok(new { Id = user.Id, UserName = user.UserName, Email = user.Email, City = user.City });
+        }
+    }
+}
---
+++ IdentityServer/KRM3D.IdentityServer/Dto/SignupDto.cs
@@ -0,0 +1,17 @@
+﻿namespace KRM3D.IdentityServer.Dto
+{
+    public class SignupDto
+    {
+      
+        public string UserName { get; set; }
+
+  
+        public string Email { get; set; }
+
+  
+        public string Password { get; set; }
+
+   
+        public string City { get; set; }
+    }
+}
